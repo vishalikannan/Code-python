4991ab5f3301ad7e7a74b218e722ea9e419b0078
harideepika <46592994+harideepika@users.noreply.github.com>
2019-01-31 21:06:20 +0530

    Create char.py

---
+++ char.py
@@ -0,0 +1,8 @@
+char=raw_input()
+if(char >= 'a' and char<='z') or (char>='A' and char<='Z'):
+        if(char =='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
+                    print("Vowel")
+        else:
+                    print("Consonant")
+      else:
+                  print("invalid")
