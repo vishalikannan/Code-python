c582faf1f412510e1e2c3d0824c9f0789f44a521
harideepika <46592994+harideepika@users.noreply.github.com>
2019-01-31 22:53:50 +0530

    Update char.py

--- char.py
+++ char.py
@@ -1,6 +1,6 @@
 char=raw_input()
 if(char >= 'a' and char<='z') or (char>='A' and char<='Z'):
-        if(char =='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
+     if(char =='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
                     print("Vowel")
         else:
                     print("Consonant")
